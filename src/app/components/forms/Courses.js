91f46951fc34f1c3806f2e824733d9ef1e9fb10c
quiloos39 <quiloos39@gmail.com>
2021-02-22 17:59:31 +0300

    Reorganization

--- src/app/components/forms/Courses.js
+++ src/app/components/forms/Courses.js
@@ -1,19 +1,42 @@
 import React from "react";
-import CourseForm from "./CourseForm";
+import Grades from "../../grades";
 
 const Courses = ({ courses, removeCourse, updateCourse }) => {
   return (
-    <div>
-      {courses.map((course) => (
-        <CourseForm
-          key={course.courseNo}
-          course={course}
-          removeCourse={removeCourse}
-          updateCourse={updateCourse}
-        />
-      ))}
-    </div>
-  );
+		<div>
+			{courses.map((course) => (
+				<div key={course.courseNo} className="row my-3">
+					<div className="col-6 mb-3">
+						<label>Course Name</label>
+						<input className="form-control" type="text" defaultValue={`Course ${course.courseNo}`} onChange={(e) => updateCourse(course, "name", e.target.value)} />
+					</div>
+					<div className="col-6 col-lg-3 mb-3">
+						<label>Credits</label>
+						<input className="form-control" type="number" defaultValue="0" onChange={(e) => updateCourse(course, "credit", Number(e.target.value))} disabled={!Grades.get(course.grade).include} />
+					</div>
+					<div className="col-lg-2 mb-3">
+						<label>Grade</label>
+						<br />
+						<div className="dropdown w-100">
+							<button className="btn btn-outline-secondary w-100 dropdown-toggle text-left" data-toggle="dropdown">
+								{course.grade}
+							</button>
+							<div className="dropdown-menu">
+								{Array.from(Grades.entries()).map((grade) => (
+									<span key={grade[0]} className="dropdown-item" onClick={() => updateCourse(course, "grade", grade[0])}>{grade[0] + (grade[1].description.length > 0 ? ` (${grade[1].description})`: "")}</span>
+								))}
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-1 mb-3 d-flex">
+						<button className="btn btn-danger mt-auto w-100" onClick={() => removeCourse(course)}>
+							<i className="fas fa-trash-alt"></i>
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Courses;
--- src/app/components/forms/Semesters.js
+++ src/app/components/forms/Semesters.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SemesterForm from "./SemesterForm";
+import Courses from "./Courses";
 
 const Semesters = ({
   semesters,
@@ -9,19 +9,25 @@ const Semesters = ({
   updateCourse,
 }) => {
   return (
-    <>
-      {semesters.map((semester) => (
-        <SemesterForm
-          key={semester.semesterNo}
-          semester={semester}
-          removeSemester={removeSemester}
-          addCourse={addCourse}
-          removeCourse={removeCourse}
-          updateCourse={updateCourse}
-        />
-      ))}
-    </>
-  );
+		<>
+			{semesters.map((semester) => (
+				<div key={semester.semesterNo} className="my-4">
+					<h2 className="text-danger">Semester {semester.semesterNo + 1}</h2>
+					<hr />
+					<Courses courses={Array.from(semester.courses.values())} addCourse={addCourse} removeCourse={removeCourse} updateCourse={updateCourse} />
+					<hr />
+					<div className="my-4">
+						<button type="button" className="btn btn-success" onClick={() => addCourse(semester.semesterNo)}>
+							Add course
+						</button>
+						<button type="button" className="btn btn-danger mx-3" onClick={() => removeSemester(semester.semesterNo)}>
+							Remove semester
+						</button>
+					</div>
+				</div>
+			))}
+		</>
+	);
 };
 
 export default Semesters;
